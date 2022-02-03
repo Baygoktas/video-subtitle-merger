11cf7b1e5ad67a069b4e53fde80ef5416db06388
hplatforms <91740193+hplatforms@users.noreply.github.com>
2022-02-03 23:17:09 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -5,9 +5,9 @@ class Translation (object):
 
     
     
-    DOWNLOAD_SUCCESS = """Dosya Başarı İle İndirildi!
+    DOWNLOAD_SUCCESS = """Dosya Başarıyla İndirildi!
 
-Geçen Süre : {} saniye."""
+Geçen Süre : {} Saniye."""
     FILE_SIZE_ERROR = "HATA : URL'den Dosya Boyutu Ayıklanamıyor!"
     MAX_FILE_SIZE = "Dosya boyutu 2 Gb'den büyük. Telegramın getirdiği sınır da bu!"
     LONG_CUS_FILENAME = """Sağladığınız dosya adı 60 karakterden büyük.
